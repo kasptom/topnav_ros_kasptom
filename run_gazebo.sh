505c677a451240da653c9cb7ce2be37651c29b33
kasptom <kasptom@gmail.com>
2018-05-19 11:42:16 +0200

    Reorganize repo

--- run_gazebo.sh
+++ run_gazebo.sh
@@ -3,5 +3,6 @@
 sudo killall rosmaster
 sudo killall gzserver
 sudo killall gzclient
-roslaunch topnav_gazebo topnav_world.launch
+#roslaunch topnav_gazebo topnav_world.launch
+roslaunch topnav_gazebo capo.launch
 
