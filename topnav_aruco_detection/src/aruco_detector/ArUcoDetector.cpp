b2118c9d3b85de65f14f2ee1f608947a172c9419
kasptom <kasptom@gmail.com>
2018-10-21 16:20:08 +0200

    Add the camera config file
          * TODO use a smoother way to include the .yaml file

--- topnav_aruco_detection/src/aruco_detector/ArUcoDetector.cpp
+++ topnav_aruco_detection/src/aruco_detector/ArUcoDetector.cpp
@@ -5,11 +5,18 @@
 #include <iostream>
 #include <boost/thread/mutex.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include "ros/ros.h"
 
 using namespace cv;
 using namespace std;
 
-ArUcoDetector::ArUcoDetector() {
+ArUcoDetector::ArUcoDetector(string cameraConfigFileName) {
+    bool initOk = init(cameraConfigFileName);
+    if (!initOk) {
+        cerr << "Invalid camera file" << endl;
+        return;
+    }
+
     camera_subscriber = handle.subscribe("capo/camera1/image_raw", 1000, &ArUcoDetector::camera_image_callback, this);
     namedWindow(ARUCO_OPENCV_WINDOW_NAME);
 }
@@ -19,29 +26,14 @@ ArUcoDetector::ArUcoDetector() {
  * @param msg
  */
 void ArUcoDetector::camera_image_callback(const sensor_msgs::Image::ConstPtr &msg) {
-    //------
     cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
+    try {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     }
-    catch (cv_bridge::Exception& e)
-    {
+    catch (cv_bridge::Exception &e) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-        cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-    // Update GUI Window
-    cv::imshow(ARUCO_OPENCV_WINDOW_NAME, cv_ptr->image);
-    cv::waitKey(3);
-
-    // Output modified video stream
-    return;
-    //------
 
     int dictionaryId = aruco::DICT_6X6_250;
     bool showRejected = false;
@@ -51,52 +43,69 @@ void ArUcoDetector::camera_image_callback(const sensor_msgs::Image::ConstPtr &ms
     detectorParameters->cornerRefinementMethod = aruco::CORNER_REFINE_SUBPIX;
 
     vector<int> ids;
-    vector< vector<Point2f> > corners, rejected;
+    vector<vector<Point2f> > corners, rejected;
     vector<Vec3d> rVectors, tVectors;
-    Mat imageCopy;
-    Mat image(msg->data);
-    Mat cameraMatrix, distortionCoefficients;
+    Mat image(cv_ptr->image);
 
     ROS_INFO("height = %d, width = %d", msg->height, msg->width);
 
-    // TODO use opencv_bridge from ROS' vision_opencv
-//    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
-//
-//    aruco::detectMarkers(image, dictionary, corners, ids, detectorParameters, rejected);
-//
-//    if (ids.size() > 0) {
-//        long numberOfMarkers = ids.size();
-//        aruco::estimatePoseSingleMarkers(corners, MARKER_LENGTH_METERS, cameraMatrix, distortionCoefficients,
-//                                         rVectors, tVectors);
-//    }
-//
-//    image.copyTo(imageCopy);
-//
-//    if (ids.size() > 0) {
-//        aruco::drawDetectedMarkers(imageCopy, corners, ids);
-//
-//        if (estimatePose) {
-//            for (int i = 0; i < ids.size(); i++) {
-//                aruco::drawAxis(imageCopy, cameraMatrix, distortionCoefficients, rVectors[i], tVectors[i],
-//                                MARKER_LENGTH_METERS * 0.5f);
-//            }
-//        }
-//    }
-//
-//    if (showRejected && rejected.size() > 0) {
-//        aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
-//    }
-//
-//    imshow("out", imageCopy);
+    try {
+        Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(
+                aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
+        aruco::detectMarkers(image, dictionary, corners, ids, detectorParameters, rejected);
+
+        long numberOfMarkers = ids.size();
+        if (numberOfMarkers > 0) {
+
+            aruco::estimatePoseSingleMarkers(
+                    corners, MARKER_LENGTH_METERS, cameraMatrix, distortionCoefficients, rVectors, tVectors);
+        }
+
+        if (numberOfMarkers > 0) {
+            aruco::drawDetectedMarkers(image, corners, ids);
+
+            if (estimatePose) {
+                for (int i = 0; i < ids.size(); i++) {
+                    aruco::drawAxis(
+                            image, cameraMatrix, distortionCoefficients, rVectors[i], tVectors[i],
+                            MARKER_LENGTH_METERS * 0.5f);
+                }
+            }
+        }
+
+        if (showRejected && rejected.size() > 0) {
+            aruco::drawDetectedMarkers(image, rejected, noArray(), Scalar(100, 0, 255));
+        }
+    } catch (cv::Exception &exception) {
+        cerr << exception.msg << endl;
+    }
+
+
+    // Update GUI Window
+    imshow(ARUCO_OPENCV_WINDOW_NAME, cv_ptr->image);
+    waitKey(3);
 }
 
 ArUcoDetector::~ArUcoDetector() {
     cv::destroyWindow(ARUCO_OPENCV_WINDOW_NAME);
 }
 
+bool ArUcoDetector::init(std::string filename) {
+    return ArUcoDetector::readCameraParameters(filename);
+}
+
+bool ArUcoDetector::readCameraParameters(std::string filename) {
+    FileStorage fs(filename, FileStorage::READ);
+    if (!fs.isOpened())
+        return false;
+    fs["camera_matrix"] >> cameraMatrix;
+    fs["distortion_coefficients"] >> distortionCoefficients;
+    return true;
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "aruco_detector");
-    ArUcoDetector detector;
+    ArUcoDetector detector("c930.yaml");
     ros::spin();
     return 0;
 }
--- topnav_aruco_detection/src/aruco_detector/ArUcoDetector.h
+++ topnav_aruco_detection/src/aruco_detector/ArUcoDetector.h
@@ -3,21 +3,29 @@
 
 #include <sensor_msgs/Image.h>
 #include <ros/node_handle.h>
+#include <opencv2/aruco.hpp>
 
 static const float MARKER_LENGTH_METERS = 0.17;
 static const std::string ARUCO_OPENCV_WINDOW_NAME = "Aruco detection preview";
 
 class ArUcoDetector {
 public:
-    ArUcoDetector();
+    ArUcoDetector(std::string cameraConfigFileName);
 
     void camera_image_callback(const sensor_msgs::Image::ConstPtr &msg);
 
     ~ArUcoDetector();
 
+    bool readCameraParameters(std::string filename);
+
 private:
+    cv::Mat cameraMatrix;
+    cv::Mat distortionCoefficients;
+
     ros::NodeHandle handle;
     ros::Subscriber camera_subscriber;
+
+    bool init(std::string fileName);
 };
 
 
