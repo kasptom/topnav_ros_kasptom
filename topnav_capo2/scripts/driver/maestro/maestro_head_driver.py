730a7b63d54ffbe2e8c59c64d527f4c1b719fc91
kasptom <kasptom@gmail.com>
2019-06-02 01:00:40 +0200

    Simplify head rotation reset

--- topnav_capo2/scripts/driver/maestro/maestro_head_driver.py
+++ topnav_capo2/scripts/driver/maestro/maestro_head_driver.py
@@ -25,9 +25,9 @@ class MaestroHeadDriver(IHeadDriver):
             return
 
         lower_target = self._MAX_TARGET - (self._MAX_TARGET - self._MIN_TARGET) * (
-                max(angle_degrees, 0) - self._MIN_ANGLE) / self._ANGULAR_SERVO_RANGE
+                max(angle_degrees, 0) - self._MID_ANGLE) / self._ANGULAR_SERVO_RANGE
         upper_target = self._MIN_TARGET + (self._MAX_TARGET - self._MIN_TARGET) * (
-                min(angle_degrees, 0) - self._MID_ANGLE) / self._ANGULAR_SERVO_RANGE
+                min(angle_degrees, 0) - self._MIN_ANGLE) / self._ANGULAR_SERVO_RANGE
 
         self._servo.setTarget(self._LOWER_SERVO_CHANNEL, lower_target)
         self._servo.setTarget(self._UPPER_SERVO_CHANNEL, upper_target)
@@ -45,8 +45,7 @@ class MaestroHeadDriver(IHeadDriver):
         return rotation
 
     def reset_head_rotation(self):
-        self._servo.setTarget(self._LOWER_SERVO_CHANNEL, self._MIN_TARGET)
-        self._servo.setTarget(self._UPPER_SERVO_CHANNEL, self._MAX_TARGET)
+        self.set_head_rotation(0)
 
     def stop_driver(self):
         self.reset_head_rotation()
