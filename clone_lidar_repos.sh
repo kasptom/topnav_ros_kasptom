75e720ceb4963924241033db5f4fdfe516ddb928
kasptom <kasptom@gmail.com>
2019-06-02 21:53:21 +0200

    Add the script for cloning LIDAR related repositories to the workspace

---
+++ clone_lidar_repos.sh
@@ -0,0 +1,6 @@
+cd ../../
+git clone https://github.com/ros-perception/laser_proc.git
+git clone https://github.com/ros-drivers/urg_c.git
+git clone https://github.com/ros-drivers/driver_common.git
+
+git clone https://github.com/ros-drivers/urg_node.git
