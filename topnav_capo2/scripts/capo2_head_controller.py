5d80400f3a5bb86f6d7a8f6f3f1f506243956506
kasptom <kasptom@gmail.com>
2019-06-07 22:32:23 +0200

    Send rotation in radians

--- topnav_capo2/scripts/capo2_head_controller.py
+++ topnav_capo2/scripts/capo2_head_controller.py
@@ -28,7 +28,7 @@ class HeadController:
         while not rospy.is_shutdown():
             head_rotation = self.driver.get_head_rotation()
             # print "publish head rotation: %.2f" % head_rotation
-            message = self._create_joint_state_messsage(head_rotation)
+            message = self._create_joint_state_messsage(head_rotation / 180.0 * math.pi)
 
             self.rotation_joint_state_publisher.publish(message)
             rate.sleep()
