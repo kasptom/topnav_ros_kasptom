4f06a8073c5cad69949002c9e07f0cd887d47844
kasptom <kasptom@gmail.com>
2019-06-23 15:36:11 +0200

    Synchronize left / right velocity setting add the scan transceiver
          ... to the master launch

--- topnav_capo2/scripts/capo2_wheels_controller.py
+++ topnav_capo2/scripts/capo2_wheels_controller.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from threading import Lock
+
 import rospy
 
 from std_msgs.msg import Float64
@@ -12,6 +14,9 @@ class CapoWheelsController:
     def __init__(self):
         self.left_wheel_velocity = 0.0
         self.right_wheel_velocity = 0.0
+        self.left_right_setting_counter = 0
+        self.lock = Lock()
+        self.left_right_counter = 0
 
         self.driver = MaestroWheelsDriver(5000, 5900, 6800, 5000, 5900, 6800)
         # self.driver = DummyWheelsDriver()
@@ -26,13 +31,22 @@ class CapoWheelsController:
         rospy.spin()
 
     def set_left_velocity(self, value):
-        self.left_wheel_velocity = value.data
-        self.driver.set_velocity(self.left_wheel_velocity, self.right_wheel_velocity)
+        with self.lock:
+            self.left_right_counter += 1
+            self.left_wheel_velocity = value.data
+            self.driver.set_velocity(self.left_wheel_velocity, self.right_wheel_velocity)
+
+            if self.left_right_counter == 0:
+                self.driver.set_velocity(self.left_wheel_velocity, self.right_wheel_velocity)
         # print("left vel: %.2f" % self.left_wheel_velocity)
 
     def set_right_velocity(self, value):
-        self.right_wheel_velocity = value.data
-        self.driver.set_velocity(self.left_wheel_velocity, self.right_wheel_velocity)
+        with self.lock:
+            self.left_right_counter -= 1
+            self.right_wheel_velocity = value.data
+
+            if self.left_right_counter == 0:
+                self.driver.set_velocity(self.left_wheel_velocity, self.right_wheel_velocity)
         # print("right vel: %.2f" % self.right_wheel_velocity)
 
 
