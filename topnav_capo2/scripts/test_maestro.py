3309835015eb4297ebdc122e093597439237cd25
kasptom <kasptom@gmail.com>
2019-06-30 14:32:45 +0200

    Add head servo test

--- topnav_capo2/scripts/test_maestro.py
+++ topnav_capo2/scripts/test_maestro.py
@@ -22,6 +22,9 @@ servo.setAccel(1, 0)
 servo.setSpeed(0, 60)
 servo.setSpeed(1, 60)
 
+servo.setSpeed(2, 12)
+servo.setSpeed(3, 12)
+
 print 'after setup'
 
 
@@ -42,6 +45,24 @@ def test_channel(channel_ids):
 test_channel([0])
 test_channel([1])
 test_channel([0, 1])
+
+
+def test_head_channels(head_channels):
+    print 'testing head servos: %s ...' % str(head_channels)
+    time.sleep(1.0)
+    print '... start ...'
+    for i in range(1, 10):
+        time.sleep(1.0)
+        for channel_id in head_channels:
+            if i % 2 == 0:
+                servo.setTarget(channel_id, 6400)
+            else:
+                servo.setTarget(channel_id, 5400)
+            print 'position %d' % servo.getPosition(channel_id)
+    print '... end'
+
+
+test_head_channels([2, 3])
 
 #   servo.setTarget(1, 5400)
 # upper = servo.getPosition(2)
