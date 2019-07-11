ceaea85f56fd2227884314fc6223d0aebb5a2315
kasptom <kasptom@gmail.com>
2019-07-12 00:27:50 +0200

    Allow to parametrize the clock publish rate via launch file

--- topnav_capo2/scripts/capo2_clock.py
+++ topnav_capo2/scripts/capo2_clock.py
@@ -5,12 +5,16 @@ import time
 from std_msgs.msg import Int64
 
 TOPIC_NAME_CAPO2_CLOCK = "capo2/clock"
+NODE_NAME = "capo2_clock"
 CAPO2_CLOCK_DEFAULT_RATE_HZ = 10
+ROS_LAUNCH_PARAM_NAME_CLOCK_PUBLISH_RATE = "/%s/clock_publish_rate_hz" % NODE_NAME
 
 
 class Capo2Clock:
     def __init__(self):
         self.publisher = rospy.Publisher(TOPIC_NAME_CAPO2_CLOCK, Int64, queue_size=1)
+        self.publish_rate = rospy.get_param(ROS_LAUNCH_PARAM_NAME_CLOCK_PUBLISH_RATE, CAPO2_CLOCK_DEFAULT_RATE_HZ)
+        print 'publishing clock with %d [Hz] rate' % self.publish_rate
 
     def start(self):
         rospy.init_node("capo2_clock_publisher", anonymous=True)
