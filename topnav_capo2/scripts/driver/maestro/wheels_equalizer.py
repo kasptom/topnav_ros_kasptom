a75540dc44a9511dd1d22e59a505dc6e8c426963
kasptom <kasptom@gmail.com>
2019-07-28 14:26:48 +0200

    Fix the proportional target calculation, update the tests

--- topnav_capo2/scripts/driver/maestro/wheels_equalizer.py
+++ topnav_capo2/scripts/driver/maestro/wheels_equalizer.py
@@ -1,9 +1,10 @@
 LEFT_WHEEL_SERVO_MAP_IDX = 0
 RIGHT_WHEEL_SERVO_AMP_IDX = 1
 
-DEFAULT_WHEELS_MAP = [(1000, 1000), (1500, 1500), (2000, 2000)]
+DEFAULT_WHEELS_MAP = [(900, 2100), (1500, 1500), (2100, 900)]
 
-CAPO_2_WHEELS_MAP = [(1200, 1795),
+CAPO_2_WHEELS_MAP = [(900, 2100),
+                     (1200, 1795),
                      (1410, 1585.5),
                      (1420, 1573.5),
                      (1430, 1566),
@@ -11,7 +12,8 @@ CAPO_2_WHEELS_MAP = [(1200, 1795),
                      (1540, 1456.75),
                      (1550, 1438),
                      (1575, 1420),
-                     (1780, 1230)]
+                     (1780, 1230),
+                     (2100, 900)]
 
 
 class WheelsEqualizer:
@@ -26,9 +28,14 @@ class WheelsEqualizer:
         right_lower_bound = left_upper_bound[RIGHT_WHEEL_SERVO_AMP_IDX]
         right_upper_bound = left_lower_bound[RIGHT_WHEEL_SERVO_AMP_IDX]
 
-        right_equalized_target = right_lower_bound + (right_upper_bound - right_lower_bound) * (
-                (symmetric_left_target - left_lower_bound[LEFT_WHEEL_SERVO_MAP_IDX]) / (
-                    left_upper_bound[LEFT_WHEEL_SERVO_MAP_IDX] - left_lower_bound[LEFT_WHEEL_SERVO_MAP_IDX]))
+        right_equalized_target = int(right_lower_bound
+                                     + (right_upper_bound - right_lower_bound)
+                                     * (1 - (symmetric_left_target - left_lower_bound[LEFT_WHEEL_SERVO_MAP_IDX])
+                                        / float(left_upper_bound[LEFT_WHEEL_SERVO_MAP_IDX]
+                                                - left_lower_bound[LEFT_WHEEL_SERVO_MAP_IDX])
+                                        ))
+        assert right_lower_bound <= right_equalized_target <= right_upper_bound
+
         return right_equalized_target
 
     @staticmethod
@@ -43,3 +50,9 @@ class WheelsEqualizer:
     def find_left_upper_bound(self, symmetric_left_target):
         return min(filter(lambda target_pair: target_pair[LEFT_WHEEL_SERVO_MAP_IDX] >= symmetric_left_target,
                           self.targets_map))
+
+
+if __name__ == '__main__':
+    equalizer = WheelsEqualizer(CAPO_2_WHEELS_MAP)
+    for target in [4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000]:
+        print (equalizer.equalize_right_target(target, 5960))
