515995d248343db906316701d46100bf5ef1c710
kasptom <kasptom@gmail.com>
2019-08-08 13:35:43 +0200

    Rename capo2_clock -> capo/clock, capo2/clock -> capo/clock

--- topnav_capo2/scripts/capo2_clock.py
+++ topnav_capo2/scripts/capo2_clock.py
@@ -4,21 +4,21 @@ import rospy
 import time
 from std_msgs.msg import UInt64
 
-TOPIC_NAME_CAPO2_CLOCK = "capo2/clock"
-NODE_NAME = "capo2_clock"
-CAPO2_CLOCK_DEFAULT_RATE_HZ = 10
+TOPIC_NAME_CAPO_CLOCK = "capo/clock"
+NODE_NAME = "capo/clock"
+CAPO_CLOCK_DEFAULT_RATE_HZ = 10
 ROS_LAUNCH_PARAM_NAME_CLOCK_PUBLISH_RATE = "/%s/clock_publish_rate_hz" % NODE_NAME
 
 
 class Capo2Clock:
     def __init__(self):
-        self.publisher = rospy.Publisher(TOPIC_NAME_CAPO2_CLOCK, UInt64, queue_size=1)
-        self.publish_rate = rospy.get_param(ROS_LAUNCH_PARAM_NAME_CLOCK_PUBLISH_RATE, CAPO2_CLOCK_DEFAULT_RATE_HZ)
+        self.publisher = rospy.Publisher(TOPIC_NAME_CAPO_CLOCK, UInt64, queue_size=1)
+        self.publish_rate = rospy.get_param(ROS_LAUNCH_PARAM_NAME_CLOCK_PUBLISH_RATE, CAPO_CLOCK_DEFAULT_RATE_HZ)
         print 'publishing clock with %d [Hz] rate' % self.publish_rate
 
     def start(self):
-        rospy.init_node("capo2_clock_publisher", anonymous=True)
-        rate = rospy.Rate(CAPO2_CLOCK_DEFAULT_RATE_HZ)
+        rospy.init_node("capo/clock/publisher", anonymous=True)
+        rate = rospy.Rate(CAPO_CLOCK_DEFAULT_RATE_HZ)
 
         while not rospy.is_shutdown():
             self.publisher.publish(time.time())
